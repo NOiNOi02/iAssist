f326e158ab51ddc19cec01849f10d85ff120c9d3
topetope024 <alcantarakristofer024@gmail.com>
2021-10-24 21:16:16 +0800

    asdasd

---
+++ lib/student/lessons/1stLaw/balancedForces.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:iassist/widget/change_theme_button_widget.dart';
+
+class BalancedForces extends StatefulWidget {
+  @override
+  _BalancedForcesState createState() => _BalancedForcesState();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _BalancedForcesState extends State<BalancedForces> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text(
+          'FIRST LAW OF MOTION',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFFBA494B),
+        elevation: 0,
+        leading: IconButton(
+          // alignment: center,
+          icon: Icon(Icons.arrow_back_rounded, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: <Widget>[ChangeThemeButtonWidget(), SizedBox(width: 25)],
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(top: 30),
+            child: Center(
+              child: Text(
+                "BALANCE FORCES",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color(0xFFBA494B),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'If two forces acting on an object are equal in magnitude but in opposite direction, they are considered balanced forces. If the forces acting on an object are balanced or its algebraic sum equates to zero, the object stays at rest.',
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 1.4,
+                  color: Color(0xFF000000),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10, left: 25, right: 25),
+            height: size.height * .19,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage('assets/images/firstLawOfMotion/3.png'),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 3, left: 25, right: 25),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Source:https://lh3.googleusercontent.com/apvOIzBv5x2fl0z6L2T4oEi5leKAWUPZGdDB33RxOQvR59LnWOpwbh-cW5XSllt-CtXTOA=s153m',
+                style: const TextStyle(
+                  fontSize: 9,
+                  letterSpacing: 1.4,
+                  color: Color(0xFF888888),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 25, right: 25),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Figure 3. A book affected by two different forces with equal magnitude and opposite direction- Balanced forces.',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 14,
+                  letterSpacing: 1.4,
+                  color: Color(0xFF000000),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Visualize a physics book on a tabletop. The book is affected by two forces. One force, Earth's gravitational attraction, pulls downward. The table's pressure on the book (sometimes called a normal force) pushes the book higher. The equal magnitude and opposite direction of these two forces balance each other. Balanced forces make the book at rest and these forces must lie along the same line. The book is deemed to be balanced. There is no uneven or unbalanced force acting on the book, therefore it stays in motion. When all the forces acting on an object are balanced, the object is at rest and does not accelerate.",
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 1.4,
+                  color: Color(0xFF000000),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10, left: 25, right: 25),
+            height: size.height * .19,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage('assets/images/firstLawOfMotion/4.png'),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 3, left: 25, right: 25),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Source: https://lh3.googleusercontent.com/6psRlTrRCa2f_cjhX5wfSdoXl1CE-RcGkQkIvSJXjuFJzP4DS6WNy9Keu6qOyZrFrWd5=s142',
+                style: const TextStyle(
+                  fontSize: 9,
+                  letterSpacing: 1.4,
+                  color: Color(0xFF888888),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 25, right: 25),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Figure 4. Showing a balanced force acting on the car.',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 14,
+                  letterSpacing: 1.4,
+                  color: Color(0xFF000000),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin:
+                const EdgeInsets.only(top: 25, left: 25, right: 25, bottom: 25),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Figure 1.4 depicts balanced forces. Balanced forces result in a force of zero. The car above, acted on by two balanced forces, remains in an equilibrium state. As a result, there is no difference in speed. In addition, balanced forces make objects to continue moving at constant velocity.",
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 1.4,
+                  color: Color(0xFF000000),
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
